b72e1755f6ec842e49df48da57542700ad1340d0
PKBO <35585905+PKBO@users.noreply.github.com>
2018-10-30 01:54:49 +0100

    Update powershell.sh

--- powershell.sh
+++ powershell.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
+ 
+echo "disabling miner"
+miner stop 
 
-echo "disabling miner"  
-miner stop   
-
-echo "installing poweshell"   
-screen -dm -S myscreenname wget https://github.com/PowerShell/PowerShell/releases/download/v6.1.0/powershell-6.1.0-linux-x64.tar.gz -O /tmp/powershell.tar.gz   
-sudo mkdir -p /opt/microsoft/powershell/6.1.0   
-sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/6.1.0   
-sudo chmod +x /opt/microsoft/powershell/6.1.0/pwsh    
-sudo ln -s /opt/microsoft/powershell/6.1.0/pwsh /usr/bin/pwsh   
-sudo rm -rf /tmp/powershell.tar.gz   
-screen -dm -S myscreenname pwsh    
-
-echo "Deleting outdated custom miner"  
-rm -rf /hive/custom  
+echo "Downloading poweshell"
+rm -rf  /tmp/powershell.tar.gz
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.1.0/powershell-6.1.0-linux-x64.tar.gz -O /tmp/powershell.tar.gz
+ 
+echo "Creating Dir for PowerShell"
+sudo mkdir -p /opt/microsoft/powershell/6.1.0
+echo "Dearchiving gzip"
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/6.1.0
+echo "Adding executable flag for pwsh"
+sudo chmod +x /opt/microsoft/powershell/6.1.0/pwsh
+echo "Creating symlink"
+sudo ln -s /opt/microsoft/powershell/6.1.0/pwsh /usr/bin/pwsh
+echo "Removing poweshell.tar.gz"
+sudo rm -rf /tmp/powershell.tar.gz
+ 
+echo "Deleting outdated custom miner"
+rm -rf /hive/custom
+rm -rf /hive/miners/custom
 mkdir /hive/custom
+mkdir /hive/miners/custom
+
+echo "Removing powershell2.sh"
+rm -rf /root/powershell2.sh
