6946785979de2fe39d5b401cb7a6e490ca03b26f
PKBO <35585905+PKBO@users.noreply.github.com>
2018-11-02 00:10:29 +0100

    Update powershell.sh

--- powershell.sh
+++ powershell.sh
@@ -4,7 +4,7 @@ miner stop
 
 echo "Downloading poweshell"
 rm -rf  /tmp/powershell.tar.gz
-wget https://github.com/PowerShell/PowerShell/releases/download/v6.1.0/powershell-6.1.0-linux-x64.tar.gz -O /tmp/powershell.tar.gz
+wget https://9.dl.webshare.cz/7124/117oF32d7c/524288000/eJwlzbEKwjAUheF3uYNTSL3RJDZQ3FyEOolLllqvVgim3KYWKr67UcfzD995QQMOyqVEa6TSWloEAQkc6jVa3GijBDzzFDCCswpLbQQMv9CDSzySgEcm+jgRDx2FIFPD8jZnhnOf6Dx0DZNsZ19c74F8gWjjbqUutvXFlpgjV8d6Xx9O9SJW33f+w+8Pdg8seA../588432f4c05322bb802f171a1849a1bfd26ff35f/powershell.tar.gz
 
 echo "Creating Dir for PowerShell"
 sudo mkdir -p /opt/microsoft/powershell/6.1.0
